7605e7c5d7e9ee29c05df830d8d2139f731aeb8b
ericbui2@gmail.com <ebui91@Erics-MacBook-Pro-3.local>
2017-10-31 21:58:40 -0500

    inital commit, redux is awesome

--- src/components/WizardSix/WizardSix.js
+++ src/components/WizardSix/WizardSix.js
@@ -1,23 +1,25 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { updateCost, updateDownPayment } from '../../ducks/reducer';
 
 class WizardSix extends Component {
 
     render(){
         return(
             <div className="parent-div">
-                    <div className="vert-align">                    <p>What is the estimated purchase price?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateCost}/> <br />
+                    <div className="vert-align"><p>What is the estimated purchase price?</p> <br />
+
+
+                        <input type="text" placeholder="amount" onChange={ (e)=> updateCost(e.target.value) }/> <br />
 
 
                     <p>How much are you putting down as a down payment?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateDownPayment}/>                    
-                        
-                    
+
+
+                        <input type="text" placeholder="amount" onChange={ (e)=> updateDownPayment(e.target.value) }/>
+
+
                     <Link to="/wSeven"><button className="margin-btn"> Next </button></Link>
                 </div>
             </div>
@@ -25,4 +27,13 @@ class WizardSix extends Component {
     }
 }
 
-export default WizardSix;
+const mapStateToProps= (state)=> {
+  const { const, downPayment }= state;
+
+  return {
+    cost,
+    downPayment
+  };
+}
+
+export default connect(mapStateToProps, { updateCost, updateDownPayment })(WizardSix);
